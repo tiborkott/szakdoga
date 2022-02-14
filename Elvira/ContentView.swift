9a73c8b8dddeb97c6399ca5905b70c7a2b7b5d5d
Tibor Kött <38768467+tiborkott@users.noreply.github.com>
2022-02-14 14:06:06 +0100

    Initial Commit

---
+++ Elvira/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Elvira
+//
+//  Created by Kött Tibor on 2022. 02. 14..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Elvira/ElviraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElviraApp.swift
+//  Elvira
+//
+//  Created by Kött Tibor on 2022. 02. 14..
+//
+
+import SwiftUI
+
+@main
+struct ElviraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
